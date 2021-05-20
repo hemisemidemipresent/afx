f3dd1f20e448ca9e56a968370809ebbdcf4d0f5c
hemisemidemipresent <hnngggrrrr@gmail.com>
2021-05-20 23:23:27 +0800

    k

---
+++ src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    let results = document.getElementsByClassName('select-selected');
+
+    artifactData.innerHTML = '';
+
+    let monkeName = results[0].innerText;
+    let durationStr = results[1].innerText;
+}
+
+function findArtifacts(ship, duration) {}
